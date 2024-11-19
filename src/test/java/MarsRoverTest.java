97cbaabb4380917328d0cb06dcc1190f8718d094
Mason ma <MAMA14@oocl.com>
2024-11-19 17:16:45 +0800

    test:should return true when  turn right

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -77,7 +77,41 @@ public class MarsRoverTest {
         assertEquals("0:0:E",report);
     }
 
+    @Test
+    public void should_face_south_when_orient_east_and_turn_right(){
+        //Given
+        MarsRover marsRover = new MarsRover();
+        //When
+        marsRover.executeCommand("R");
+        String report = marsRover.executeCommand("R");
+        //Then
+        assertEquals("0:0:S",report);
+    }
 
+    @Test
+    public void should_face_west_when_orient_south_and_turn_right(){
+        //Given
+        MarsRover marsRover = new MarsRover();
+        //When
+        marsRover.executeCommand("R");
+        marsRover.executeCommand("R");
+        String report = marsRover.executeCommand("R");
+        //Then
+        assertEquals("0:0:W",report);
+    }
+
+    @Test
+    public void should_face_north_when_orient_west_and_turn_right(){
+        //Given
+        MarsRover marsRover = new MarsRover();
+        //When
+        marsRover.executeCommand("R");
+        marsRover.executeCommand("R");
+        marsRover.executeCommand("R");
+        String report = marsRover.executeCommand("R");
+        //Then
+        assertEquals("0:0:N",report);
+    }
 
 
 }
