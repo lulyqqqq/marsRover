9b848718653718810ee93c445b11d28916661149
Mason ma <MAMA14@oocl.com>
2024-11-19 17:08:11 +0800

    test:should return true when orient north and turn left face west

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,6 +1,13 @@
 public class MarsRover {
     private String direction = "N";
+
     public String showStatus() {
+        return "0:0:"+direction;
+    }
+
+
+    public String executeCommand(String command) {
+
         return "0:0:"+direction;
     }
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -3,6 +3,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MarsRoverTest {
+
+    @Test
+    public void testMarsRover(){
+        assertTrue(true);
+    }
+
     @Test
     public void should_initialize_mars_rover(){
         //Given
@@ -12,4 +18,19 @@ public class MarsRoverTest {
         //Then
         assertEquals("0:0:N",report);
     }
+
+    // turn left
+    @Test
+    public void should_face_west_when_orient_north_and_turn_left(){
+        //Given
+        MarsRover marsRover = new MarsRover();
+        //When
+        String report = marsRover.executeCommand("L");
+        //Then
+        assertEquals("0:0:W",report);
+    }
+
+
+
+
 }
