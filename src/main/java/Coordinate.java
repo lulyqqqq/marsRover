3a3d27cd2188762f118e49f7b6d7f6aef7685b12
Mason ma <MAMA14@oocl.com>
2024-11-20 21:16:52 +0800

    refactor:add coordinate(x,y) and Instruction

---
+++ src/main/java/Coordinate.java
@@ -0,0 +1,22 @@
+public class Coordinate {
+    public int x;
+    public int y;
+
+    public Coordinate(){
+        x = 0;
+        y = 0;
+    }
+
+    public Coordinate(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int X(){
+        return x;
+    }
+
+    public int Y(){
+        return y;
+    }
+}
